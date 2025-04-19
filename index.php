f70aea3fac7a235ac4d696f2548b08dfe929e322
Adrián Calvete <adriancalvetedev@gmail.com>
2025-04-19 18:41:02 +0200

    Globales de CSS creados en el archivo _globales.scss

--- index.php
+++ index.php
@@ -3,10 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Click Salud</title>
+    <title>ClickSalud</title>
+    <link rel="icon" href="build/img/iconoClickSalud.png" type="image/png">
     <link rel="stylesheet" href="/build/css/styles.css">
 </head>
 <body>
-    
+    <header class="header">
+        <div class="contenedor">
+            <div class="header-nav">
+                <a href="/">
+                    <img src="build/img/logoClickSalud.png" alt="Logotipo de Click salud">
+                </a>
+                <nav class="header-nav_enlaces">
+                    <a href="pages/nosotros.php">Nosotros</a>
+                    <a href="pages/servicios.php">Servicios</a>
+                    <a href="pages/blog.php">Blog</a>
+                    <a href="pages/contacto.php">Contacto</a>
+                    <a href="pages/faq.php">FAQ</a>
+                    <a href="pages/login.php">Iniciar sesión</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <h1>Titulo de la página</h1>
+    </main>
+
+    <footer class="footer">
+        <div class="contenedor contenedor-footer">
+            <nav class="header-nav_enlaces">
+                <a href="pages/nosotros.php">Nosotros</a>
+                <a href="pages/servicios.php">Servicios</a>
+                <a href="pages/blog.php">Blog</a>
+                <a href="pages/contacto.php">Contacto</a>
+                <a href="pages/faq.php">FAQ</a>
+                <a href="pages/login.php">Iniciar sesión</a>
+            </nav>
+        </div>
+
+        <p class="licencia">ClickSalud&copy;. Todos los derechos reservados <?php date('Y/m/d') ?> </p>
+    </footer>
 </body>
 </html>
