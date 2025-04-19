312733d63c15bc3a8d453a9c25a7357592aa091e
Adrián Calvete <adriancalvetedev@gmail.com>
2025-04-19 20:05:08 +0200

    Templates de header y footer con estilos ya definidos desde SASS y fichero base.php

---
+++ includes/templates/footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <div class="contenedor contenedor-footer">
+            <nav class="header-nav_enlaces">
+                <a href="pages/nosotros.php">Nosotros</a>
+                <a href="pages/servicios.php">Servicios</a>
+                <a href="pages/blog.php">Blog</a>
+                <a href="pages/contacto.php">Contacto</a>
+                <a href="pages/faq.php">FAQ</a>
+                <a href="pages/login.php">Iniciar sesión</a>
+            </nav>
+        </div>
+
+        <p class="licencia">ClickSalud&copy; <?php echo date('Y') ?>. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClickSalud</title>
+    <link rel="icon" href="build/img/iconoClickSalud.png" type="image/png">
+    <link rel="stylesheet" href="/build/css/styles.css">
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor-header">
+            <div class="header-nav">
+                <a class="header-logo" href="/">
+                    <img src="build/img/logoClickSaludSmall.png" alt="Logotipo de Click salud">
+                </a>
+                <nav class="header-nav_enlaces">
+                    <a href="pages/nosotros.php">Nosotros</a>
+                    <a href="pages/servicios.php">Servicios</a>
+                    <a href="pages/blog.php">Blog</a>
+                    <a href="pages/contacto.php">Contacto</a>
+                    <a href="pages/faq.php">FAQ</a>
+                    <a class="header-nav_enlaces-login" href="pages/login.php">Iniciar sesión</a>
+                </nav>
+            </div>
+        </div>
+    </header>
