d28bcd5200a1d215b2cdc72cea5cb37b61b3aea6
Adrián Calvete <adriancalvetedev@gmail.com>
2025-04-20 17:58:15 +0200

    Vista de la página interna "Nosotros" finalizada

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,11 +1,11 @@
 <footer>
         <div class="contenedor contenedor-footer">
             <nav class="header-nav_enlaces">
-                <a href="pages/nosotros.php">Nosotros</a>
-                <a href="pages/servicios.php">Servicios</a>
-                <a href="pages/blog.php">Blog</a>
-                <a href="pages/contacto.php">Contacto</a>
-                <a href="pages/faq.php">FAQ</a>
+                <a href="../pages/nosotros.php">Nosotros</a>
+                <a href="../pages/servicios.php">Servicios</a>
+                <a href="../pages/blog.php">Blog</a>
+                <a href="../pages/contacto.php">Contacto</a>
+                <a href="../pages/faq.php">FAQ</a>
             </nav>
         </div>
 
---
+++ pages/nosotros.php
@@ -0,0 +1,27 @@
+<?php
+    include '../includes/templates/header.php';
+?>
+
+    <main class="contenedor">
+        <h1>Conoce más acerca de ClickSalud</h1>
+
+        <div class="nosotros">
+            <div class="nosotros-imagen">
+                <img src="../build/img/programador.png" alt="">
+            </div>
+            <div class="nosotros-texto">
+                <blockquote>Estudiante de Desarrollo Aplicaciones Web</blockquote>
+                
+                <p>
+                    ClickSalud es un proyecto que nace como entrega para el proyecto final de curso de los estudios cursados en el Ciclo Superior en Desarrollo de Aplicaciones Web recibido de manera online en la Universidad Alfonso X El sabio.
+                    En este proyecto se busca poner en práctica todo el aprendizaje recibido entre los cursos 2023/2024 y 2024/2025 poniendo sobre el desarrollo del mismo concepto de programación en frontend y backend, diseñando las interfaces con un concepto "responsive", con un producto que busca la usabilidad del usuario.
+                    En la parte de backend se ha implantado la arquitectura de una base de datos conectada a un lenguaje de programación de backend (PHP) que sirve dichos datos al frontend para poder mostrarselos al usuario.
+                    Además, se han implementado sesiones de usuario con un sistema de login y la posibilidad de que ciertos usuarios puedan crear y actualizar datos en la base de datos, creando así un CRUD completo.
+                </p>
+            </div>
+        </div>        
+    </main>
+
+<?php
+    include '../includes/templates/footer.php';
+?>
